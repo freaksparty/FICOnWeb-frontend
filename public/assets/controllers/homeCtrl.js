84edb88121a3aa8463daab61a3567c2c5fac79e7
xiromoreira <xiromoreira@gmail.com>
2015-11-25 00:28:53 +0100

    Merge news calls

--- public/assets/controllers/homeCtrl.js
+++ public/assets/controllers/homeCtrl.js
@@ -1,12 +1,13 @@
 (function () {
     var homeCtrl = function ($scope, $rootScope, $http, $cookieStore, $routeParams, $location) {
         $scope.view = {};
-		$scope.data = {};
-		$scope.desc = {};
+		//$scope.data = {};
+		//$scope.desc = {};
 		$scope.currentPage = 1;
 		$scope.pagination = 10;
-		$scope.size = 0;
-		$scope.data.tournaments = [];
+		//$scope.size = 0;
+		//$scope.data.tournaments = [];
+		$scope.news = {};
 		
 		$scope.isNext = function () {
 			return $scope.currentPage * $scope.pagination < $scope.size;
@@ -22,7 +23,7 @@
             $location.url("home?page=" + $scope.currentPage);
 		}
 		
-		$scope.getSize = function () {	
+		/*$scope.getSize = function () {	
 			if ($cookieStore.get('FICOnCookie')) {
 				$http({
 					url: $rootScope.config.apiUrl + '/api/event/news/published/size/1',
@@ -36,28 +37,26 @@
 			} else {
 				console.log('error');
 			}
-		}
+		}*/
 		
 		
 		$scope.getNews = function () {	
-			if ($cookieStore.get('FICOnCookie')) {
-				$http({
-					url: $rootScope.config.apiUrl + '/api/event/news/published/1/' + $scope.currentPage + '/' + $scope.pagination,
-					method: "GET",
-					headers: { "sessionId" :  $cookieStore.get('FICOnCookie').sessionId }
-				}).success(function (data, status, headers, config) {
-					$scope.data = data;
-				}).error(function (data, status, headers, config) {
-					console.log('error get');
-				});
-			} else {
-				console.log('error');
-			}
+			$http({
+				url: $rootScope.config.apiUrl + '/api/event/' + $rootScope.config.eventId + 
+				    '/news/published/' + $scope.currentPage + '/' + $scope.pagination,
+				method: "GET",
+				cache: true,
+				headers: { "sessionId" :  $cookieStore.get('FICOnCookie').sessionId }
+			}).success(function (data, status, headers, config) {
+				$scope.news = data;
+			}).error(function (data, status, headers, config) {
+				console.log('error get');
+			});
 		}
 		
         $scope.ctr = function () {
             if ($routeParams.page) $scope.currentPage = $routeParams.page;
-			$scope.getSize();
+			//$scope.getSize();
 			$scope.getNews();
 			$rootScope.getEventData($scope);
 		};
