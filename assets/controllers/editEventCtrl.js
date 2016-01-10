a096d2e61f7b61de3f0c8c3bdddae4cb0ae0a649
Siro González <xiromoreira@gmail.com>
2016-01-10 02:16:55 +0100

    Event edition with inscription opening and close times

--- assets/controllers/editEventCtrl.js
+++ assets/controllers/editEventCtrl.js
@@ -5,22 +5,26 @@
 	$scope.view.event.name = "";
 	$scope.view.event.description = "";
 	$scope.view.event.normas = "";
-	$scope.view.event.registrationOpenDate = "";
+	$scope.view.event.registrationOpenDate;
+	$scope.view.event.registrationCloseDate;
+	$scope.view.registrationOpenDate;
+	$scope.view.registrationCloseDate;
 	
 	$scope.changeDate = function (date) {
 		return moment(date).format('DD-MM-YYYY/HH:mm:ss'); 
 	}
 	
 	$scope.update = function() {
-		if(!$scope.view.event.registrationOpenDate) {
-		    var d = new Date();
-		    $scope.view.event.registrationOpenDate = d;
+		if(!$scope.view.registrationOpenDate) {
+		    $scope.view.registrationOpenDate = new Date();
+		}
+		if(!$scope.view.registrationCloseDate) {
+		    $scope.view.registrationCloseDate = new Date();
 		}
 	};
 	
-	$scope.openInscriptionStart = function($ev) {
-		$ev.preventDefault();$ev.stopPropagation();$scope.openedInscriptionStart=true;
-	};
+	$scope.openInscriptionStart = function($ev) {$ev.preventDefault();$ev.stopPropagation();$scope.openedInscriptionStart=true;};
+	$scope.openInscriptionEnd = function($ev) {$ev.preventDefault();$ev.stopPropagation();$scope.openedInscriptionClose=true;};
 	
 	$scope.open = function($event) {
 		$event.preventDefault();
@@ -37,7 +41,8 @@
 	$scope.format = 'dd-MM-yyyy';
 	
 	$scope.send = function (event) {
-		$scope.view.event.registrationOpenDate = null;
+		$scope.view.event.registrationOpenDate = $scope.changeDate($scope.view.registrationOpenDate);
+		$scope.view.event.registrationCloseDate = $scope.changeDate($scope.view.registrationCloseDate);
 		if($routeParams.id) {
 			$rootScope.postUri('/api/event/'+$routeParams.id, $scope.view.event, function(){$location.path("/home");}, undefined, 'PUT');
 		} else {
@@ -52,7 +57,8 @@
 				$scope.view.event.normas = data.rules;
 				$scope.view.event.description = data.description;
 				$scope.view.event.registrationOpenDate = data.openInscriptionDate;
-				//$scope.view.event.registrationOpenDate = null;
+				$scope.view.registrationOpenDate = moment(data.openInscriptionDate, 'DD-MM-YYYY/HH:mm:ss').toDate();
+				$scope.view.registrationCloseDate = moment(data.closeInscriptionDate, 'DD-MM-YYYY/HH:mm:ss').toDate();
 				$scope.update();
 			});
 		} else {
