b9cb74369abc73b1f40daaee273b396fbf85bf26
xiromoreira <xiromoreira@gmail.com>
2016-01-11 23:24:29 +0100

    Admin menu detecting roles

--- assets/controllers/menuCtrl.js
+++ assets/controllers/menuCtrl.js
@@ -1,33 +1,41 @@
 (function () {
     var menuCtrl = function ($scope, $http, $cookieStore, $rootScope, $window, $location) {
-		$scope.data = {};
-		$scope.data.tournaments = [];
-		$scope.data.productions = [];
-		$scope.data.conferences = [];
-        	
-		$scope.logout = function () {
-			if ($cookieStore.get('FICOnCookie')) {
-				$http({
-					url: $rootScope.config.apiUrl + '/api/session',
-					method: "DELETE",
-					headers: { "sessionId" :  $cookieStore.get('FICOnCookie').sessionId }
-				}).success(function (data, status, headers, config) {
-					$cookieStore.remove('FICOnCookie');
-					$location.path('/');
-					$window.location.reload();
-				}).error(function (data, status, headers, config) {
-					$cookieStore.remove('FICOnCookie');
-					$location.path('/');
-					$window.location.reload();
-				});
-			} else {
-				$rootScope.createSession();
-			}
-		};
+	$scope.data = {};
+	$scope.data.tournaments = [];
+	$scope.data.productions = [];
+	$scope.data.conferences = [];
+	
+	$scope.logout = function () {
+		if ($cookieStore.get('FICOnCookie')) {
+			$http({
+				url: $rootScope.config.apiUrl + '/api/session',
+				method: "DELETE",
+				headers: { "sessionId" :  $cookieStore.get('FICOnCookie').sessionId }
+			}).success(function (data, status, headers, config) {
+				$cookieStore.remove('FICOnCookie');
+				$location.path('/');
+				$window.location.reload();
+			}).error(function (data, status, headers, config) {
+				$cookieStore.remove('FICOnCookie');
+				$location.path('/');
+				$window.location.reload();
+			});
+		} else {
+			$rootScope.createSession();
+		}
+	};
 		
         $scope.ctr = function () {
-			$rootScope.getEventData($scope);
+		$rootScope.getEventData($scope);
         };
+
+	$scope.checkRole = function(role) {
+		return $scope.vars.roles.indexOf(role) != -1;
+	}
+
+	$scope.showAdmin = function() {
+		return $scope.checkRole('Admin') || $scope.checkRole('ActivityController') || $scope.checkRole('EventController') || $scope.checkRole('NewsController') || $scope.checkRole('SponsorController') || $scope.checkRole('RegistrationController');
+	}
 
         $scope.ctr();
     }
